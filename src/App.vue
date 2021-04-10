feb0a38f0415482be15393620e4bbe14529913b3
Yaoyoyo <964862992@qq.com>
2021-04-10 20:19:41 +0800

    初始化调整目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <h1>黑马后台发布管理系统</h1>
+    <!-- 路由出口 -->
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'App'
+}
+</script>
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+// 创建Vue根实例
+// 把router配置到根实例中
+// 通过 render 方法把 根组件 App 渲染到 #app入口节点
 new Vue({
   router,
   render: h => h(App)
